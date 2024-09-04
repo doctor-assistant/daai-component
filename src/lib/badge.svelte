43d70c818433a89f8edaf92057d4dbf6e78c81ef
vitoria.cst <vitoria.cst.holanda@gmail.com>
2024-09-04 15:52:01 -0300

    test

--- src/lib/badge.svelte
+++ src/lib/badge.svelte
@@ -19,7 +19,7 @@
     <button
       type="button"
       class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-      >Pausar Registro</button
+      >Pausar Registroooo</button
     >
   </div>
 </div>
