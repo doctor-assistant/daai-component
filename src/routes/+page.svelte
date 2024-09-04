6b3a8d97ec502a88522eefc6addb416a674c68fb
vitoria.cst <vitoria.cst.holanda@gmail.com>
2024-09-04 20:56:50 -0300

    build: :wrench: config svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,25 @@
 <script>
-  import '../app.css';
+	import '../app.css';
+	const logo = '/logo.png';
 </script>
 
 <div class="w-full h-screen flex items-center justify-center">
-  <div
-    class="text-gray-700 flex items-center  w-1/2 gap-4 p-4 bg-gray-300 rounded-lg"
-  >
-    <button
-      type="button"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >Iniciar Registro</button
-    >
-    <button
-      type="button"
-      class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-      >Finalizar Registro</button
-    >
-    <button
-      type="button"
-      class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-      >Pausar Registro</button
-    >
-  </div>
+	<div class="text-gray-700 flex items-center gap-4 p-4 bg-gray-300 rounded-lg">
+		<img src={logo} alt="daai-logo" />
+		<button
+			type="button"
+			class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+			>Iniciar Registro</button
+		>
+		<button
+			type="button"
+			class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+			>Finalizar Registro</button
+		>
+		<button
+			type="button"
+			class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+			>Pausar Registro</button
+		>
+	</div>
 </div>
