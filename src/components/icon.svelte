c0881a4f42e8429ba9190c5f2c2eeb43eb9b7011
vitoria.cst <vitoria.cst.holanda@gmail.com>
2024-09-05 12:39:07 -0300

    style: :lipstick: add font awesome and creating the buttons

---
+++ src/components/icon.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let icon = 'fas fa-home';
+</script>
+
+<i class={icon}></i>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,23 +23,21 @@
 <div class="w-full h-screen flex items-center justify-center">
 	<div class="text-gray-700 flex items-center gap-4 p-4 border-2 border-primary rounded-lg">
 		<img src={logo} alt="daai-logo" />
-		<div></div>
-		<button
-			type="button"
-			class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-		>
-			<img src={pauseIcon} alt="daai-logo" />
+		<button type="button" class="bg-recording text-white px-5 py-2.5 rounded-lg">
+			<i class="fa-solid fa-pause"></i>
 		</button>
-		<button
-			type="button"
-			class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-			>Iniciar Registro</button
-		>
-		<button
-			type="button"
-			class="text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-			>Finalizar Registro</button
-		>
+		<button type="button" class="bg-primary text-white px-5 py-2.5 rounded-lg">
+			<i class="fa-solid fa-microphone"></i>
+			Iniciar Registro
+		</button>
+		<button type="button" class="bg-recording text-white px-5 py-2.5 rounded-lg">
+			<i class="fa-solid fa-check"></i>
+			Finalizar Registro
+		</button>
+		<button type="button" class="bg-primary text-white px-5 py-2.5 rounded-lg">
+			<i class="fa-solid fa-circle"></i>
+			Continuar registro
+		</button>
 	</div>
 </div>
 
