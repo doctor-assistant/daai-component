9bab32e08c5ba583781386c647abe078ef272f36
vitoria.cst <vitoria.cst.holanda@gmail.com>
2024-09-11 20:58:00 -0300

    refactor: :recycle: adjusting the recording logic and adding an animation

--- src/daai-badge.js
+++ src/daai-badge.js
@@ -1,20 +1,25 @@
 class DaaiBadge extends HTMLElement {
   constructor() {
     super();
+    // Variáveis de estado e objetos relacionados ao áudio
     this.mediaRecorder = null;
     this.chunks = [];
     this.stream = null;
     this.audioContext = null;
+    this.analyser = null;
+    this.gainNode = null;
     this.status = 'waiting';
     this.devices = [];
     this.currentDeviceId = null;
 
+    // Aqui criamos a shadow dom
     const shadow = this.attachShadow({ mode: 'open' });
-
+    // Aqui criamos o style
     const style = document.createElement('style');
+
     style.textContent = `
-     @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css');
-     @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+      @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css');
+      @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
       .container {
         display: flex;
         align-items: center;
@@ -25,27 +30,28 @@ class DaaiBadge extends HTMLElement {
       .recorder-box {
         display: flex;
         align-items: center;
-        justify-content:center;
-        gap: 2rem;
+        justify-content: center;
+        text-items:center;
+        gap: 1rem;
         padding: 1rem;
-        border: 3px solid  #009CB1;
+        border: 3px solid #009CB1;
         border-radius: 30px;
         background-color: #ffffff;
         height: 60px;
         width: 600px;
         font-family: "Inter", sans-serif;
+        font-weight: 500;
+        position: relative;
       }
       .recorder-box img {
         height: 40px;
       }
       .recorder-box button {
-        diplay:flex
-        width:300px;
-        height:50px;
+        height: 50px;
         padding: 0.5rem 1rem;
         border: none;
         border-radius: 8px;
-        font-size: medium;
+        font-size: 15px;
         cursor: pointer;
         transition: transform 0.15s ease-in-out;
       }
@@ -53,24 +59,25 @@ class DaaiBadge extends HTMLElement {
         transform: scale(0.95);
       }
       .button-primary {
-        width:300px;
-        height:50px;
-        font-size:30px;
-        border-radius:8px;
+        height: 50px;
+        font-size: 30px;
+        border-radius: 8px;
         background-color: #009CB1;
         color: white;
       }
-      .button-pause{
+      .button-pause {
         width: 100px;
-        height: Fill (29.99px)px;
+        height: 50px;
         gap: 15px;
-        opacity: 0px;
+        opacity: 1;
+        background-color: #F43F5E;
+        color:white;
       }
       .button-recording {
-        width:200px;
-        height:50px;
-        font-size:30px;
-        border-radius:8px;
+        width: 300px;
+        height: 50px;
+        font-size: 30px;
+        border-radius: 8px;
         background-color: #F43F5E;
         color: white;
       }
@@ -78,7 +85,6 @@ class DaaiBadge extends HTMLElement {
         background-color: #28a745;
         color: white;
       }
-
       .hidden {
         display: none;
       }
@@ -89,17 +95,18 @@ class DaaiBadge extends HTMLElement {
         transform: translate(-50%, -50%);
         background-color: #fff;
         border: 1px solid #ccc;
-        border-radius:10px;
+        border-radius: 10px;
         padding: 1rem;
         z-index: 1000;
         display: none;
+        font-family: "Inter", sans-serif;
+
       }
       .modal.active {
         display: block;
       }
       .modal button {
         margin-top: 1rem;
-
       }
       .backdrop {
         position: fixed;
@@ -107,34 +114,53 @@ class DaaiBadge extends HTMLElement {
         left: 0;
         width: 100vw;
         height: 100vh;
-        background: rgba(0, 0, 0, 0.5);
         z-index: 999;
         display: none;
-        border-radius:20px;
+        border-radius: 20px;
       }
       .backdrop.active {
         display: block;
       }
-      .close-button{
-        width:200px;
+      .close-button {
+        width: 200px;
         background-color: #009CB1;
-        border:none;
-        border-radius:4px;
-        padding:4px;
-        color:#FFFFFF;
-        }
-      .button-change{
+        border: none;
+        border-radius: 4px;
+        padding: 4px;
+        color: #FFFFFF;
+      }
+      .button-change {
         background-color: transparent;
-        font-size:30px;
-        border:none;
-        padding:4px;
-        color:#64748B;
+        font-size: 30px;
+        border: none;
+        padding: 4px;
+        color: #64748B;
         width: 12.66px;
         height: 13.26px;
-
+      }
+      .button-resume{
+        width:250px;
+        height: 50px;
+        font-size: 30px;
+        border-radius: 8px;
+        background-color: #009CB1;
+        color: white;
+      }
+    .audio-visualizer {
+      width: 300px;
+      height: 100px;
+      pointer-events: none;
+      background-color: transparent;
+    }
+    .audio-visualizer.recording {
+        margin-bottom: 50px;
       }
 
-
+    .button[disabled] {
+      cursor: not-allowed;
+      background-color:#A6AFC366;
+      opacity: 0.5;
+    }
     `;
 
     const container = document.createElement('div');
@@ -152,26 +178,30 @@ class DaaiBadge extends HTMLElement {
     this.statusText.textContent = 'Aguardando autorização do microfone';
     this.recorderBox.appendChild(this.statusText);
 
-    this.buttons = {
-      changeMicrophone: this.createButton('change', "fas fa-gear fa-lg", '', this.openMicrophoneModal.bind(this)),
-      start: this.createButton('start', 'fa fa-microphone', 'Iniciar Registro', this.startRecording.bind(this)),
-      pause: this.createButton('pause', 'fa fa-pause', '', this.pauseRecording.bind(this)),
-      finish: this.createButton('finish', 'fa  fa-check', 'Finalizar Registro', this.finishRecording.bind(this)),
-      resume: this.createButton('resume', ' fa fa-circle', 'Continuar Registro', this.resumeRecording.bind(this)),
-      download: this.createButton('download', 'fa fa-download', 'Download Registro', this.downloadRecording.bind(this)),
-    };
-
-    for (const button of Object.values(this.buttons)) {
-      this.recorderBox.appendChild(button);
+    if(this.status !== 'waiting'){
+      this.canvas = document.createElement('canvas');
+      this.canvas.className = 'audio-visualizer';
+      this.recorderBox.appendChild(this.canvas);
     }
 
+// aqui vamos usar o createButton para criar esses botões com ícones e textos apropriados.
+    this.buttons = {
+      changeMicrophone: this.createButton('change', 'fas fa-gear fa-lg', '', this.openMicrophoneModal.bind(this)),
+      start: this.createButton('start', 'fa fa-microphone', 'Iniciar Registro', this.startRecording.bind(this)),
+      pause: this.createButton('pause', 'fa fa-pause', '', this.pauseRecording.bind(this)),
+      finish: this.createButton('finish', 'fa fa-check', 'Finalizar Registro', this.finishRecording.bind(this)),
+      resume: this.createButton('resume', 'fa fa-circle', 'Continuar Registro', this.resumeRecording.bind(this)),
+      download: this.createButton('download', 'fa fa-download', 'Download', this.downloadRecording.bind(this))
+    };
 
+    Object.values(this.buttons).forEach(button => this.recorderBox.appendChild(button));
+    // aqui foi construido o modal para a troca de microfones
     this.modal = document.createElement('div');
     this.modal.className = 'modal';
     this.modal.innerHTML = `
-      <h2>Selecionar Microfone</h2>
+      <p>Escolha o Microfone</p>
       <select id="microphone-select"></select>
-      <button id="close-modal" class='close-button'>Fechar</button>
+      <button id="close-modal" class="close-button">Fechar</button>
     `;
 
     this.backdrop = document.createElement('div');
@@ -187,52 +217,65 @@ class DaaiBadge extends HTMLElement {
     this.loadDevices();
   }
 
+  // metódo para criar os botões, definido o seu conteúdo
   createButton(type, iconClass, text, handler) {
     const button = document.createElement('button');
     button.className = `button ${this.getButtonClass(type)}`;
     button.innerHTML = `<i class="fa-solid ${iconClass}"></i> ${text}`;
     button.addEventListener('click', handler);
-    button.classList.add('hidden');
     return button;
   }
 
+  // setando as classes de acordo com o status
   getButtonClass(type) {
-    console.log('type',type)
     if (type === 'start') return 'button-primary';
     if (type === 'pause') return 'button-pause';
     if (type === 'finish') return 'button-recording';
-    if (type === 'resume') return 'button-primary';
+    if (type === 'resume') return 'button-resume';
     if (type === 'download') return 'button-success';
-    if(type === 'change') return 'button-change'
+    if (type === 'change') return 'button-change';
     return '';
   }
 
+// aqui foi criado a lógica de alterar os botões de acordo com o status, ex: se for paused o botão de pause e resume vão ser renderizados
   updateButtons() {
     Object.keys(this.buttons).forEach(buttonType => {
       const button = this.buttons[buttonType];
+
       switch (this.status) {
         case 'waiting':
-          buttonType === 'start' || buttonType === 'changeMicrophone' ? button.classList.remove('hidden') : button.classList.add('hidden');
-          break;
         case 'authorized':
+          buttonType === 'start' || buttonType === 'changeMicrophone' ? button.classList.remove('hidden') : button.classList.add('hidden');
+          button.disabled = false;
+          break;
         case 'paused':
-          buttonType === 'pause' || buttonType === 'start' || buttonType === 'changeMicrophone' ? button.classList.remove('hidden') : button.classList.add('hidden');
+          buttonType === 'pause' || buttonType === 'resume' ? button.classList.remove('hidden') : button.classList.add('hidden');
+          this.canvas.classList.remove('recording');
+          if (buttonType === 'pause') {
+            button.disabled = true;
+          } else {
+            button.disabled = false;
+          }
           break;
         case 'recording':
           buttonType === 'pause' || buttonType === 'finish' ? button.classList.remove('hidden') : button.classList.add('hidden');
+          this.canvas.classList.add('recording');
+          button.disabled = false;
           break;
         case 'finished':
           buttonType === 'download' ? button.classList.remove('hidden') : button.classList.add('hidden');
+          button.disabled = false;
           break;
       }
     });
   }
 
+// aqui nesse ele manipula os microfones
   async loadDevices() {
     try {
       const devices = await navigator.mediaDevices.enumerateDevices();
       this.devices = devices.filter(device => device.kind === 'audioinput');
-
+      // aqui ele seleciona o microfone
       const select = this.modal.querySelector('#microphone-select');
       select.innerHTML = '';
       this.devices.forEach(device => {
@@ -241,11 +284,11 @@ class DaaiBadge extends HTMLElement {
         option.textContent = device.label || `Microfone ${device.deviceId}`;
         select.appendChild(option);
       });
-
+      // adiciona o evento para ouvir caso mude e seta para o novo
       select.addEventListener('change', () => {
         this.currentDeviceId = select.value;
       });
-
+      // lógica para fechar o modal
       const closeModal = this.modal.querySelector('#close-modal');
       closeModal.addEventListener('click', () => {
         this.closeMicrophoneModal();
@@ -266,6 +309,7 @@ class DaaiBadge extends HTMLElement {
   }
 
   async startRecording() {
+    this.statusText.textContent = '';
     try {
       const constraints = { audio: { deviceId: this.currentDeviceId ? { exact: this.currentDeviceId } : undefined } };
       this.stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -274,29 +318,93 @@ class DaaiBadge extends HTMLElement {
       }
 
       const source = this.audioContext.createMediaStreamSource(this.stream);
-      source.connect(this.audioContext.destination);
+      this.analyser = this.audioContext.createAnalyser();
+      this.analyser.fftSize = 256;
+      const bufferLength = this.analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      this.gainNode = this.audioContext.createGain();
+      source.connect(this.analyser);
+      this.analyser.connect(this.gainNode);
+
+      this.gainNode.connect(this.audioContext.createBufferSource().context.destination);
 
       this.mediaRecorder = new MediaRecorder(this.stream);
       this.mediaRecorder.ondataavailable = (event) => this.handleDataAvailable(event);
       this.mediaRecorder.onstop = () => this.handleStop();
       this.mediaRecorder.start();
       this.status = 'recording';
-      this.statusText.textContent = 'Gravando';
+
+      this.setupVisualizer(this.analyser, dataArray, bufferLength);
 
       this.updateButtons();
     } catch (error) {
       console.error('Erro ao acessar o microfone:', error);
       this.statusText.textContent = 'Erro ao acessar o microfone';
-      this.status = 'waiting'; // Retorna ao status inicial
+      this.status = 'waiting';
       this.updateButtons();
     }
   }
+
+  setupVisualizer(analyser, dataArray, bufferLength) {
+    const canvas = this.canvas;
+    const ctx = canvas.getContext('2d');
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+    const barWidth = (WIDTH / bufferLength) * 1.5;
+    const barSpacing = 4;
+    let barHeight;
+    let x = 0;
+
+    const scale = 0.5;
+
+    const draw = () => {
+      if (this.status === 'recording' || this.status === 'paused' || this.status === 'resume') {
+        requestAnimationFrame(draw);
+
+        analyser.getByteFrequencyData(dataArray);
+
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+        if (this.status === 'paused') {
+          ctx.strokeStyle = '#009CB1';
+          ctx.lineWidth = 2;
+          ctx.setLineDash([3, 3]);
+          ctx.beginPath();
+          ctx.moveTo(0, HEIGHT / 2);
+          ctx.lineTo(WIDTH, HEIGHT / 2);
+          ctx.stroke();
+
+        } else {
+          ctx.fillStyle = '#F43F5E';
+          x = 0;
+
+          for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] * scale;
+
+            ctx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+
+            x += barWidth + barSpacing;
+          }
+        }
+      } else {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+      }
+    };
+
+    draw();
+  }
+
+
 
   pauseRecording() {
     if (this.mediaRecorder && this.mediaRecorder.state === 'recording') {
       this.mediaRecorder.pause();
       this.status = 'paused';
-      this.statusText.textContent = 'Registro pausado';
+
+      if (this.gainNode) {
+        this.gainNode.gain.value = 0;
+      }
       this.updateButtons();
     }
   }
@@ -305,7 +413,11 @@ class DaaiBadge extends HTMLElement {
     if (this.mediaRecorder && this.mediaRecorder.state === 'paused') {
       this.mediaRecorder.resume();
       this.status = 'recording';
-      this.statusText.textContent = 'Gravando';
+
+      if (this.gainNode) {
+        this.gainNode.gain.value = 1;
+      }
+
       this.updateButtons();
     }
   }
@@ -326,7 +438,6 @@ class DaaiBadge extends HTMLElement {
   handleStop() {
     this.recordingBlob = new Blob(this.chunks, { type: 'audio/wav' });
     this.chunks = [];
-    this.statusText.textContent = 'Pronto para download';
   }
 
   downloadRecording() {
