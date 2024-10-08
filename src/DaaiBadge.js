3d6cbeefc4db3726337d4ad3a0d78bac1769ee2c
vitoria.cst <vitoria.cst.holanda@gmail.com>
2024-10-08 11:15:23 -0300

    refactor: :recycle: separating style.css

--- src/DaaiBadge.js
+++ src/DaaiBadge.js
@@ -12,7 +12,6 @@ import { createButton } from './utils/CreateButtons.js';
 import { initializeEasterEgg } from './utils/EasterEgg.js';
 import { finishRecording, pauseRecording, resumeRecording, startRecording } from './utils/RecorderUtils.js';
 
-
 class DaaiBadge extends HTMLElement {
   constructor() {
     super();
@@ -29,210 +28,16 @@ class DaaiBadge extends HTMLElement {
     this.intervalId = null;
     this.easterEggTimeoutId = null;
     blockPageReload()
-
     // Aqui criamos a shadow dom
     const shadow = this.attachShadow({ mode: 'open' });
     // Aqui criamos o style
     const style = document.createElement('style');
 
-    this.shadowRoot.innerHTML = `
-   <style>
-      @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
-      .container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100vw;
-        margin-top:20px
-      }
-      .recorder-box {
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        padding: 1rem;
-        border: 3px solid;
-        border-radius: 30px;
-        background-color: #ffffff;
-        height: 60px;
-        width: 700px;
-        font-family: "Inter", sans-serif;
-        font-weight: 600;
-        position: relative;
-        color: var(--text-badge-color, #009CB1);
-      }
-      .recorder-box button {
-        height: 50px;
-        padding: 0.5rem 1rem;
-        border: none;
-        border-radius: 8px;
-        font-size: 15px;
-        cursor: pointer;
-        transition: transform 0.15s ease-in-out;
-      }
-      .recorder-box button:active {
-        transform: scale(0.95);
-      }
-      .text-waiting-mic{
-        color:#F43F5E;
-      }
-      .text-finish {
-         margin-right:120px
-       }
-      .text-upload {
-          color: var(--text-badge-color, #009CB1);
-       }
-      .text-waiting-mic-aprove {
-        color:#F43F5E;
-         font-weight: bold;
-      }
-      .button-primary {
-        height: 50px;
-        font-size: 30px;
-        border-radius: 8px;
-        background-color:var(--button-start-recording-color, #009CB1);
-        color: white;
-        display: flex;
-        justify-content:center;
-        align-items:center;
-        gap:2px;
-      }
-      .button-pause {
-        width: 60px;
-        height: 50px;
-        opacity: 1;
-        background-color: #F43F5E;
-        color:white;
-      }
-      .button-recording {
-        height: 50px;
-        font-size: 30px;
-        border-radius: 8px;
-        background-color: #F43F5E;
-        color: white;
-      }
-      .button-success {
-        background-color: #28a745;
-        color: white;
-      }
-      .hidden {
-        display: none;
-      }
-      .modal {
-        position: fixed;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #fff;
-        border: 1px solid #ccc;
-        border-radius: 10px;
-        padding: 1rem;
-        z-index: 1000;
-        display: none;
-        font-family: "Inter", sans-serif;
-      }
-      .modal.active {
-        display: block;
-      }
-      .modal button {
-        margin-top: 1rem;
-      }
-      .backdrop {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
-        z-index: 999;
-        display: none;
-        border-radius: 20px;
-      }
-      .backdrop.active {
-        display: block;
-      }
-      .close-button {
-        width: 200px;
-        background-color:#525252;
-        border: none;
-        border-radius: 4px;
-        padding: 4px;
-        color: #FFFFFF;
-      }
-      .button-change {
-        background-color: transparent;
-        font-size: 30px;
-        border: none;
-        padding: 4px;
-        color: #64748B;
-        width: 12.66px;
-        height: 13.26px;
-      }
-      .button-resume{
-        height: 50px;
-        font-size: 30px;
-        border-radius: 8px;
-        background-color: ${this.getAttribute('button-resume-color')};
-        color: white;
-      }
-    .audio-visualizer {
-      pointer-events: none;
-      background-color: transparent;
-    }
-    .audio-hide {
-      width:0px;
-      heigth:0px
-    }
+    const linkElement = document.createElement('link');
+    linkElement.setAttribute('rel', 'stylesheet');
+    linkElement.setAttribute('href', 'src/style.css');
 
-    .button[disabled] {
-      cursor: not-allowed;
-      background-color:#A6AFC366;
-      opacity: 0.5;
-    }
-
-    .text-waiting-aprove-mic {
-      color: var(--text-badge-color, #009CB1);
-    }
-
-    .animation-mic-test{
-      width:130px;
-      margin-top:40px;
-    }
-
-    .rounded-select {
-     font-size: 18px;
-      padding: 10px 16px;
-      border-radius: 25px;
-      border: 1px solid #ccc;
-      background-color: #fff;
-      font-size: 16px;
-      outline: none;
-      appearance: none;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      background-repeat: no-repeat;
-      background-position: right 10px center;
-      background-size: 16px;
-      cursor: pointer;
-  }
-  .modal-title{
-      color:#000000
-  }
-  .rounded-select:focus {
-    border-color: #007bff;
-  }
-    .select-button {
-      border-radius: 10px;
-      width: 600px;
-      height: 50px;
-      text-align: center;
-      text-align-last: center;
-      font-family: "Inter", sans-serif;
-      font-weight: 500;
-  }
-    .timer {
-      font-weight: 600;
-      color: #000000;
-    }
-  ;
-  `;
+    shadow.appendChild(linkElement);
 
     const container = document.createElement('div');
     container.className = 'container';
@@ -352,10 +157,9 @@ class DaaiBadge extends HTMLElement {
     } else {
       this.theme = defaultTheme;
     }
-
-    // Passa o contexto `this` para a função `applyThemeAttributes`
     applyThemeAttributes(this.theme, this);
   }
+
   // aqui foi criado a lógica de alterar os botões de acordo com o status, ex: se for paused o botão de pause e resume vão ser renderizados
   updateButtons() {
     Object.keys(this.buttons).forEach((buttonType) => {
