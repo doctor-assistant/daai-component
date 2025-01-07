53d1a2fbe4f8ac0ec469837914070966ca282239
Jadson Souza <jadson@doctorassistant.ai>
2025-01-07 19:02:28 -0300

    feat: add webpack and change imports

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+/** @type {import('webpack').Configuration} */
+const config = {
+    target: 'webworker',
+    entry: './src/DaaiBadge.js',
+    output: {
+        path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist'),
+        filename: 'daai-badje.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]',
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: 'commonjs vscode',
+    },
+    resolve: {
+        mainFields: ['browser', 'module', 'main'],
+        extensions: ['.ts', '.js'],
+        alias: {
+            'eventsource': 'extended-eventsource',
+            'dexie': 'dexie',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(path.dirname(fileURLToPath(import.meta.url)), 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        liveReload: true,
+        hot: false,
+    },
+};
+
+export default config;
